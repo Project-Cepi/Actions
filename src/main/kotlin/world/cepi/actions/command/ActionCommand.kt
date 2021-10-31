94f19cb3f3d2b25278ed14354bad18fc3cfda742
LeoDog896 <LeoDog896@hotmail.com>
2021-10-31 14:12:46 -0400

    Add literal

--- src/main/kotlin/world/cepi/actions/command/ActionCommand.kt
+++ src/main/kotlin/world/cepi/actions/command/ActionCommand.kt
@@ -2,11 +2,12 @@ package world.cepi.actions.command
 
 import world.cepi.actions.list.actions
 import world.cepi.kstom.command.arguments.generation.GeneratedArguments.Companion.createSyntaxesFrom
+import world.cepi.kstom.command.arguments.literal
 import world.cepi.kstom.command.kommand.Kommand
 
 object ActionCommand : Kommand({
     actions.forEach { clazz ->
-        createSyntaxesFrom(clazz) { action ->
+        createSyntaxesFrom(clazz, clazz.simpleName!!.dropLast("Action".length).literal()) { action ->
             action.invoke(player, player)
         }
     }
