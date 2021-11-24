b22ef7ae29352273259707bc5e98d6a95a2e3269
LeoDog896 <LeoDog896@hotmail.com>
2021-11-24 14:18:37 -0500

    New event system

--- src/main/kotlin/world/cepi/actions/command/subcommands/EventSubcommand.kt
+++ src/main/kotlin/world/cepi/actions/command/subcommands/EventSubcommand.kt
@@ -1,36 +1,31 @@
 package world.cepi.actions.command.subcommands
 
-import net.minestom.server.entity.Entity
 import net.minestom.server.entity.Player
-import net.minestom.server.event.Event
-import net.minestom.server.event.EventNode
+import world.cepi.actions.Action
+import world.cepi.actions.ActionItem
 import world.cepi.actions.actionItem
 import world.cepi.kstom.command.arguments.literal
 import world.cepi.kstom.command.kommand.Kommand
 
-class EventNodeTargetMap<T : Event>(val node: EventNode<T>, val mapper: T.() -> Pair<Entity, Entity>)
+data class ActionEventHandler<T>(
+    val name: String,
+    val grabList: Player.() -> List<ActionItem>,
+    val listToItem: Player.(List<ActionItem>) -> T
+)
 
-open class EventSubcommand(
+open class EventSubcommand<T>(
     name: String = "event",
     val eventCondition: Kommand.SyntaxContext.() -> Boolean = { true },
-    val eventNodes: List<Pair<(Player) -> EventNodeTargetMap<out Event>?, String>>
+    val eventNodes: List<ActionEventHandler<T>>
 ) : Kommand({
 
     eventNodes.forEach { targetLambdaPair ->
 
-        syntax(targetLambdaPair.second.literal()) {
+        syntax(targetLambdaPair.name.literal()) {
 
             if (!eventCondition(this)) return@syntax
 
-            val target = targetLambdaPair.first(this.player) ?: return@syntax
-
-            val item = player.itemInMainHand.actionItem ?: return@syntax
-
-            target as EventNodeTargetMap<Event>
-
-            target.node.addListener(Event::class.java) {
-                item(target.mapper(it).first, target.mapper(it).second)
-            }
+            targetLambdaPair.listToItem(player, targetLambdaPair.grabList(player) + (player.itemInMainHand.actionItem ?: return@syntax))
         }
     }
 
