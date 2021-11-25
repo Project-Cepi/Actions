8fc013a51ba1d907819c2d05ac90ecf1d1751d90
LeoDog896 <LeoDog896@hotmail.com>
2021-11-25 17:55:27 -0500

    Fix up display nam

--- src/main/kotlin/world/cepi/actions/ActionItem.kt
+++ src/main/kotlin/world/cepi/actions/ActionItem.kt
@@ -1,6 +1,8 @@
 package world.cepi.actions
 
 import kotlinx.serialization.Serializable
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.entity.Entity
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
@@ -14,6 +16,18 @@ data class ActionItem(
     val timing: ActionTiming? = null
 ) {
     fun renderItem() = item(Material.RED_DYE) {
+
+        displayName(
+            Component.text(Regex("[A-Z][a-z]+")
+                .findAll(action::class.simpleName!!)
+                .map { it.value }.joinToString(" "), NamedTextColor.RED)
+        )
+
+        if (timing != null) lore(
+            Component.empty(),
+            Component.text("Repeats every ${timing.repeatEvery.seconds}s, ${timing.repeatAmount} times")
+        )
+
         this["action", ActionSerializer.module] = this@ActionItem
     }
 
