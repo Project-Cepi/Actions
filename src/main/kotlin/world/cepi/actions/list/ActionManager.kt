1ccca45cfdaff00cef1b15216f3b21599777e4a7
LeoDog896 <LeoDog896@hotmail.com>
2021-11-26 19:19:40 -0500

    Encapsulation

--- src/main/kotlin/world/cepi/actions/list/ActionManager.kt
+++ src/main/kotlin/world/cepi/actions/list/ActionManager.kt
@@ -3,14 +3,20 @@ package world.cepi.actions.list
 import world.cepi.actions.Action
 import kotlin.reflect.KClass
 
-object ActionManager : MutableList<KClass<out Action>> by mutableListOf(
-    SoundAction::class,
-    NearbySoundAction::class,
-    FlingAction::class,
-    RemoveAction::class,
-    MessageAction::class
-) {
-    fun add(vararg classes: KClass<out Action>) = addAll(classes)
+object ActionManager : Iterable<KClass<out Action>> {
+
+    override fun iterator() = list.iterator()
+
+    private val list = mutableListOf(
+        SoundAction::class,
+        NearbySoundAction::class,
+        FlingAction::class,
+        RemoveAction::class,
+        MessageAction::class
+    )
+
+    fun add(vararg classes: KClass<out Action>) = list.addAll(classes)
+    fun remove(clazz: KClass<out Action>) = list.remove(clazz)
 
     inline fun <reified T: Action> add() = add(T::class)
 }
