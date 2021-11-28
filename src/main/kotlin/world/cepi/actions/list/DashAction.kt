d31a828c94fcf10f436b16faae3dcbecb105b152
LeoDog896 <LeoDog896@hotmail.com>
2021-11-27 20:20:45 -0500

    Fix dash action

--- src/main/kotlin/world/cepi/actions/list/DashAction.kt
+++ src/main/kotlin/world/cepi/actions/list/DashAction.kt
@@ -1,12 +1,21 @@
 package world.cepi.actions.list
 
+import kotlinx.serialization.Serializable
+import net.kyori.adventure.sound.Sound
 import net.minestom.server.entity.Entity
+import net.minestom.server.entity.Player
+import net.minestom.server.sound.SoundEvent
 import world.cepi.actions.Action
+import world.cepi.kstom.command.arguments.generation.annotations.DefaultNumber
 
-class DashAction(val power: Double) : Action() {
+@Serializable
+data class DashAction(
+    val power: Double = 15.0
+) : Action() {
 
     override fun invoke(source: Entity, target: Entity?) {
         source.velocity = source.position.direction().normalize().mul(power)
+        (source as? Player)?.playSound(Sound.sound(SoundEvent.ENTITY_PLAYER_ATTACK_SWEEP, Sound.Source.MASTER, 1f, 1f))
     }
 
 }
