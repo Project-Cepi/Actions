8e7f75e6a564296aa5c01067c39271888b978953
LeoDog896 <LeoDog896@hotmail.com>
2021-12-13 17:33:21 -0500

    Fix teleport serialization

--- src/main/kotlin/world/cepi/actions/list/TeleportAction.kt
+++ src/main/kotlin/world/cepi/actions/list/TeleportAction.kt
@@ -1,10 +1,16 @@
 package world.cepi.actions.list
 
+import kotlinx.serialization.Serializable
 import net.minestom.server.entity.Entity
 import net.minestom.server.utils.location.RelativeVec
 import world.cepi.actions.Action
+import world.cepi.kstom.serializer.RelativeVecSerializer
 
-class TeleportAction(val position: RelativeVec) : Action() {
+@Serializable
+class TeleportAction(
+    @Serializable(with = RelativeVecSerializer::class)
+    val position: RelativeVec
+) : Action() {
 
     override fun invoke(source: Entity, target: Entity?) {
         source.teleport(position.from(source.position).asPosition())
