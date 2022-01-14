96b9c1b6059511fe196ac63217c1221a62c038a6
LeoDog896 <LeoDog896@gmail.com>
2022-01-14 15:51:30 -0500

    Load Actions after initialization

--- src/main/kotlin/world/cepi/actions/ActionsExtension.kt
+++ src/main/kotlin/world/cepi/actions/ActionsExtension.kt
@@ -1,15 +1,19 @@
 package world.cepi.actions
 
 import net.minestom.server.extensions.Extension;
+import net.minestom.server.utils.time.TimeUnit
 import world.cepi.actions.command.ActionCommand
+import world.cepi.kstom.Manager
 import world.cepi.kstom.util.log
 import world.cepi.kstom.util.node
 
 class ActionsExtension : Extension() {
 
     override fun initialize(): LoadStatus {
-        // TODO register actions command after initialization
-        ActionCommand.register()
+        // Register the action command after the server is loaded
+        Manager.scheduler.buildTask {
+            ActionCommand.register()
+        }.delay(1, TimeUnit.SERVER_TICK).schedule()
 
         log.info("[Actions] has been enabled!")
 
